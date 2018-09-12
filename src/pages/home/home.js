2d75b25af7c2f654b37d4047abb9aebff2347c6a
sanya-shch <shchipkov.sanya@ukr.net>
2018-09-12 14:03:41 +0300

    Adding Functionality

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,93 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import Input from '../../components/ui/input/index';
+import { bindAll } from 'lodash';
+import { connect } from 'react-redux';
+import { addTodo, likeTodo, deleteTodo } from './actions';
+import classnames from 'classnames';
+import './styles.less';
 
-export default class HomePage extends React.Component {
+class HomePage extends React.Component {
     
     static path = '/';
+    static propTypes = {
+        home: PropTypes.object.isRequired,
+        dispatch: PropTypes.func.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoName: ''
+        };
+
+        bindAll(this, ['renderTodos', 'inputOnChange', 'addTodo']);
+    }
+
+    inputOnChange(value) {
+        this.setState({ todoName: value });
+    }
+
+    addTodo() {
+        const { todos } = this.props.home;
+        const id = todos[todos.length - 1].id + 1;
+        const name = this.state.todoName;
+        this.props.dispatch( addTodo(id, name) );
+        this.setState({ todoName: ''});
+    }
+
+    renderTodos(item, idx) {
+        const todoClasses = classnames('b-home-todo', {
+            'is-liked': item.liked
+        });
+        const btnClasses = classnames('btn', {
+            'active': item.liked
+        });
+        return (
+            <li key={ idx }>
+                <span className={ todoClasses }>{ item.name }</span> 
+                <button className='btn' onClick={ this.deleteTodo.bind(this, item) }><i className='glyphicon glyphicon-remove' /></button>
+                <button className={ btnClasses } onClick={ this.likeTodo.bind(this, item) }><i className='glyphicon glyphicon-heart' /></button>
+            </li>
+        );
+    }
+
+    deleteTodo(todo) {
+        this.props.dispatch( deleteTodo(todo) );
+    }
+
+    likeTodo(todo) {
+        this.props.dispatch( likeTodo(todo) );
+    }
     
     render() {
+        const { todoName } = this.state;
+        const { todos, error } = this.props.home;
         return (
-            <div>
-                <div className="alert alert-success">Hi</div>
+            <div className='row-fluid b-home'>
+                <div className='col-xs-12'>
+                    <ul>
+                        { todos.map(this.renderTodos) }
+                    </ul>
+                    <div className='col-xs-4'>
+                        <Input
+                            onChange={ this.inputOnChange }
+                            value={ todoName }
+                            error={ error }
+                        />
+                        <button className='btn btn-primary b-home-submit' onClick={ this.addTodo }>Add todo</button>
+                    </div>
+                </div>
             </div>
         );
     }
     
 }
+
+function mapStateToProps(state) {
+    return {
+        home: state.home  
+    };
+}
+
+export default connect(mapStateToProps)(HomePage);
